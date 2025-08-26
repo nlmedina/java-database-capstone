c5799a26a1167ee2d8b55ab41e33dfaf1df86b5d
Nico Medina <nico@nlmedina.com>
2025-08-26 16:19:49 -0700

    added Admin model

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -1,5 +1,15 @@
 package com.project.back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Admin {
 
 // @Entity annotation:
@@ -34,4 +44,15 @@ public class Admin {
 // 5. Getters and Setters:
 //    - Standard getter and setter methods are provided for accessing and modifying the fields.
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Username cannot be null")
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
